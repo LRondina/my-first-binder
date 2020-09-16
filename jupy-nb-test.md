b9fc1e82ae049bf85f011d2982a3539944fcc93b
Luca Rondina <rondina.luc@gmail.com>
2020-09-16 18:46:28 +0100

    Added notebook (various formats)

---
+++ jupy-nb-test.md
@@ -0,0 +1,19 @@
+```python
+# Test python version
+!python --version
+```
+
+    Python 3.7.8
+
+
+### Jupyter notebook test
+This is a simple Jupyter notebook to test the [mybinder](https://mybinder.org/) service.
+
+
+```python
+# Classic Hello, World!
+print( "Hello Binder!" )
+```
+
+    Hello Binder!
+
---
+++ jupy-nb-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Test python version
+get_ipython().system('python --version')
+
+
+# ### Jupyter notebook test
+# This is a simple Jupyter notebook to test the [mybinder](https://mybinder.org/) service.
+
+# In[3]:
+
+
+# Classic Hello, World!
+print( "Hello Binder!" )
+
